9e601ed21407ae0818c5e51ef20f839bc4d71b27
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-04-18 10:57:14 +0530

    [priyanshu]modified valid password

--- Program.cs
+++ Program.cs
@@ -6,46 +6,48 @@ namespace RegularExpressions
     class Program
     {
         static void Main(string[] args)
-        {      
+        {     
 
-            Console.WriteLine("Welcome To User Registration");
+            Console.WriteLine("-------Welcome To User Registration--------");
             Console.WriteLine();
-            string Pattern = "^([a-z]{3,})([.]{0,1}[a-z]*)@([a-z]{2}).([a-z]{2})([.]{1}[a-z]{2}){0,1}$"; //pattern using for email.
+            string pattern = "^[A-Za-z0-9]{8,}$"; //pattern for password minimum 8 char.
 
             Program p = new Program(); // create object
-            p.ValidName(Pattern);
+            p.ValidPass(pattern);
             Console.WriteLine();
             Console.Read();
         }
 
-        public void ValidName(string Pattern) //method to validate string
+        public void ValidPass(string pattern) //method to validate string
         {
-            Console.WriteLine("Validation Of The Email");
-            Regex regex = new Regex(Pattern);
+
+            Console.WriteLine("Validation Of The Password");
+            Regex regex = new Regex(pattern);
             IterateLoop(regex);  //calling method
         }
 
         public void IterateLoop(Regex regex)
         {
-            int i = 0; 
+            int i = 0; //initialize i value
             while (i != 1)
             {
-                Console.WriteLine("Enter Email");
-                string email = Console.ReadLine();
-                bool result = regex.IsMatch(email);  //call the IsMatch metod to determine whether a match is present
+                Console.WriteLine("Enter Password");
+                string Password = Console.ReadLine();
+                bool result = regex.IsMatch(Password);  //call the IsMatch metod to determine whether a match is present
 
                 if (result == true)  ////check result is true or not using if and hence using bool
                 {
-                    Console.WriteLine("Valid email");
+                    Console.WriteLine("Valid password");
                     i = 1;
                 }
                 else
                 {
-                    Console.WriteLine("Enter email in this format : abc.xyz@bl.co.in");
+                    Console.WriteLine("Enter minimum eight character");
                 }
 
             }
         }
+
     }
 }
 
