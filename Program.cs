f758e961c0babeaad2b56c5bde29f38711db1b50
PriyanshuSingh-5 <80380590+PriyanshuSingh-5@users.noreply.github.com>
2021-04-30 08:06:34 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace RegularExpressions
             Console.WriteLine("-------Welcome To User Registration--------");
             Console.WriteLine();
             string pattern = "^([A-Za-z\\d-_\\+]+)(\\.[A-Za-z\\d-_]+)?@([a-zA-Z\\d]+)\\.([a-zA-Z]{2,4})(\\.[A-Za-z]{2,4})?$"; //pattern for email sample 
-                                                                                                                              // ASSIGING SOME VALID SAMPLES
+                                                                                                                              
             string[] inputs = { "abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com", "abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com", "abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com", "abc123@.com.com",
                               ".abc@abc.com", "abc()*@gmail.com", "abc@%*.com", "abc..2002@gmail.com", "abc.@gmail.com",
                                 "abc@abc@gmail.com", "abc@gmail.com.1a", "abc@gmail.com.aa.au" };
